d24c40072c4bfd6dbc287c9653e9ef7c4de127bd
grozamorei <desperatebassplayer@gmail.com>
2018-10-29 14:44:06 +0300

    mockup landscape generator

--- src/ts/grid/LandscapeGenerator.ts
+++ src/ts/grid/LandscapeGenerator.ts
@@ -1,15 +1,34 @@
 import {Utils} from "../utils/Utils";
 import {HexTemplate} from "../gen/HexTemplate";
+import {Facade} from "../Facade";
 
 export module grid {
 
   export class LandscapeGenerator {
 
-    private readonly weights: number[] = [0.3, 0.15, 0.2, 0.15, 0.1, 0.1]
+    private static readonly weights: number[] = [0.3, 0.15, 0.2, 0.15, 0.1, 0.1]
 
-    constructor(width: number, height: number) {
-      const possibleTypes = Utils.getEnumOptions(HexTemplate.Values)
-      console.log(possibleTypes)
+    static weightedRandomLayout(width: number, height: number): HexTemplate[] {
+      const result = []
+      const strVariations = Utils.getEnumOptionsStr(HexTemplate.Values)
+      const variations = Utils.getEnumOptions(HexTemplate.Values)
+
+      const mapSize = width * height
+      const approximatedWeights = this.weights.map(w => Math.ceil(w * mapSize))
+      const extraTiles = approximatedWeights.reduce((acc, v) => acc + v, 0) - mapSize
+      if (extraTiles > 0) {
+        for (let i = 0; i < extraTiles; i++) {
+          approximatedWeights[i] -= 1
+        }
+      }
+
+      variations.forEach(v => {
+          for (let i = 0; i < approximatedWeights[v]; i++) {
+            result.push(Facade.$.cdb.getHexTemplate(strVariations[v]))
+          }
+      })
+
+      return result.sort((a, b) => -1 + Math.random() * 2)
     }
   }
 }
