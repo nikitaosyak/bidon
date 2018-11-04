500f1b3877145ab6bcfb54dc84cd618aead91605
grozamorei <desperatebassplayer@gmail.com>
2018-11-04 14:39:06 +0300

    added mrdoob's stats

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,20 @@
 import {Facade} from "./Facade";
 import {Events} from "./events";
 import {Grid} from "./grid/Grid";
+import Stats = require('Stats.js')
 
 window.onload = () => {
 
+  const stats = new Stats()
+  stats.showPanel(0)
+  document.body.appendChild(stats.dom)
+
   const gameLoop = () => {
+    stats.begin()
+
     Facade.$.update(10)
+
+    stats.end()
     requestAnimationFrame(gameLoop)
   }
 
